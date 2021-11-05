c82208822bd85476e5613787538ab29a7f84ba44
Marina Monsivais <minamonsi@me.com>
2021-11-05 11:53:23 -0500

    WIP

--- api/product/products.test.js
+++ api/product/products.test.js
@@ -2,6 +2,13 @@ const request = require('supertest')
 const db = require('../../data/db-config')
 const server = require('../server')
 
+const product1 = {makeup_type: 'foundation', brand_name: 'Revlon'}
+const product2 = {makeup_type: 'primer', brand_name: 'Elf'}
+
+beforeAll(async ()=>{
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
 
 it('correct env var', ()=>{
     expect(process.env.DB_ENV).toBe("testing")
