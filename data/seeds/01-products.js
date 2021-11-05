c6c9f7a62ce250a9696c2c6d09508ad3c3e3594f
Marina Monsivais <minamonsi@me.com>
2021-11-05 00:01:06 -0500

    Created seed file

---
+++ data/seeds/01-products.js
@@ -0,0 +1,20 @@
+exports.seed = function(knex, Promise){
+    return knex('products')
+    .truncate()
+    .then(function() {
+        return knex('products').insert([
+            { 
+                makeup_type: 'foundation',
+                brand_name: 'Revlon'
+            },
+            { 
+                makeup_type: 'primer',
+                brand_name: 'Elf'
+            },
+            { 
+                makeup_type: 'powder',
+                brand_name: 'Maybelline'
+            },
+        ])
+    })
+}
